9f3a1eaf23aff3a4a5c6aca1fafc811e41f3372a
Riko <xxy1836@gmail.com>
2025-05-30 18:01:14 +0800

    2025-5-30 work2

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,138 @@
-use iced::Center;
-use iced::widget::{Column, button, column, text};
+use iced::Background;
+use iced::Border;
+use iced::Color;
+use iced::Length;
+use iced::Size;
+use iced::Theme;
+use iced::alignment::Horizontal;
+use iced::alignment::Vertical;
+use iced::widget::Button;
+use iced::widget::Column;
+use iced::widget::Container;
+use iced::widget::Row;
+use iced::widget::Text;
+use iced::widget::TextInput;
+use iced::widget::container;
+use iced::window;
+use iced_aw::MenuBar;
+use iced_aw::menu_items;
+
+pub fn style_box_1(theme: &Theme) -> container::Style {
+    let palette = theme.extended_palette();
+
+    container::Style {
+        background: Some(Background::Color(Color::from_rgba(0.0, 0.0, 0.0, 0.0))),
+        border: Border {
+            width: 1.0,
+            radius: 0.0.into(),
+            color: palette.background.strong.color,
+        },
+        ..container::Style::default()
+    }
+}
 
 pub fn main() -> iced::Result {
-    let title = "Counter Example";
-    iced::application(title, Counter::update, Counter::view).run()
+    let mut window = window::Settings::default();
+    let icon = window::icon::from_file("./assets/corgi.png").expect("can not load icon");
+    window.icon = Some(icon);
+    window.size = Size::new(800.0, 600.0);
+    iced::application(App::title, App::update, App::view)
+        .window(window)
+        .run()
 }
 
 #[derive(Default)]
-struct Counter {
-    value: i64,
+struct App {
+    scan_info: String,
+    target_ip: String,
+    target_port: String,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 enum Message {
-    Increment,
-    Decrement,
+    StartScan,
+    UpdateIP(String),
+    UpdatePort(String),
 }
 
-impl Counter {
+impl App {
+    fn title(&self) -> String {
+        format!("{} v{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"))
+    }
     fn update(&mut self, message: Message) {
         match message {
-            Message::Increment => {
-                self.value += 1;
+            Message::StartScan => {
+                self.scan_info =
+                    format!("Starting scan on {}:{}", self.target_ip, self.target_port);
             }
-            Message::Decrement => {
-                self.value -= 1;
+            Message::UpdateIP(ip) => {
+                self.target_ip = ip;
+            }
+            Message::UpdatePort(port) => {
+                self.target_port = port;
             }
         }
     }
-
     fn view(&self) -> Column<Message> {
-        column![
-            button("Increment").on_press(Message::Increment),
-            text(self.value).size(50),
-            button("Decrement").on_press(Message::Decrement)
-        ]
-        .padding(20)
-        .align_x(Center)
+        let roots = menu_items![(Text::new("File"))(Text::new("Help"))];
+        let menu_bar = MenuBar::new(roots);
+
+        let input_ip_text = Text::new("Target")
+            .line_height(1.0)
+            .align_x(Horizontal::Center);
+
+        let input_ip = TextInput::new("127.0.0.1", &self.target_ip)
+            .on_input(Message::UpdateIP)
+            .padding(10)
+            .line_height(1.0)
+            .width(Length::Fill)
+            .align_x(Horizontal::Left);
+
+        let input_port_text = Text::new("Port")
+            .line_height(1.0)
+            .align_x(Horizontal::Center);
+
+        let input_port = TextInput::new("80", &self.target_port)
+            .on_input(Message::UpdatePort)
+            .padding(10)
+            .line_height(1.0)
+            .width(Length::Fill)
+            .align_x(Horizontal::Left);
+
+        let start_button_text = Text::new("Start")
+            .line_height(1.0)
+            .width(Length::Fill)
+            .align_x(Horizontal::Center);
+        let start_button = Button::new(start_button_text)
+            .width(Length::Fill)
+            .on_press(Message::StartScan);
+
+        let row_1 = Row::new()
+            .padding(20)
+            .spacing(20)
+            .align_y(Vertical::Center)
+            .push(input_ip_text)
+            .push(input_ip)
+            .push(input_port_text)
+            .push(input_port)
+            .push(start_button);
+
+        let group_1 = Container::new(row_1)
+            .width(Length::Fill)
+            .align_x(Horizontal::Center)
+            .align_y(Vertical::Center)
+            .style(style_box_1);
+
+        let target_check_line = Text::new(&self.scan_info)
+            .width(Length::Fill)
+            .align_x(Horizontal::Left);
+
+        Column::new()
+            .padding(20)
+            .spacing(20)
+            .align_x(Horizontal::Center)
+            .push(group_1)
+            .push(target_check_line)
+            .into()
     }
 }
