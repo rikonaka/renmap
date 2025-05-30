17eb2d67a66f46c411570a8d96a7626bfacf8757
Riko <xxy1836@gmail.com>
2025-05-30 12:00:40 +0800

    Init codes and license

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,41 @@
-fn main() {
-    MainWindow::new().unwrap().run().unwrap();
+use iced::Center;
+use iced::widget::{Column, button, column, text};
+
+pub fn main() -> iced::Result {
+    let title = "Counter Example";
+    iced::application(title, Counter::update, Counter::view).run()
 }
 
-slint::slint! {
-    export component MainWindow inherits Window {
-        Text {
-            text: "hello world";
-            color: green;
+#[derive(Default)]
+struct Counter {
+    value: i64,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    Increment,
+    Decrement,
+}
+
+impl Counter {
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::Increment => {
+                self.value += 1;
+            }
+            Message::Decrement => {
+                self.value -= 1;
+            }
         }
+    }
+
+    fn view(&self) -> Column<Message> {
+        column![
+            button("Increment").on_press(Message::Increment),
+            text(self.value).size(50),
+            button("Decrement").on_press(Message::Decrement)
+        ]
+        .padding(20)
+        .align_x(Center)
     }
 }
